f8e4780d6678f8ef52291b3db919ebaf9d876801
Lester Ag Padul <padulleste@gmail.com>
2015-10-05 15:51:40 +0800

    fdc [update] added comment

--- inc/core/ErrorHandler.php
+++ inc/core/ErrorHandler.php
@@ -3,6 +3,7 @@ class ErrorHandler{
 	function __construct(){
 	}
 
+	// write log file on error
 	public function writeError($err = FALSE){
 		// check if err is valid
 		if ($err == FALSE) {
--- inc/inc.php
+++ inc/inc.php
@@ -1,9 +1,12 @@
 <?php 
+// set the uses array
 $uses = array(
 	$_SERVER['DOCUMENT_ROOT'].'/nc_ad_devel/inc/FDCWebhook.php',
 	$_SERVER['DOCUMENT_ROOT'].'/nc_ad_devel/config/const.php',
 	$_SERVER['DOCUMENT_ROOT'].'/nc_ad_devel/inc/core/ErrorHandler.php'
 );
+
+// count and check if uses array is valid
 if (count($uses) !== 0 && is_array($uses) !== FALSE) {
 	foreach ($uses as $dependency) {
 		require_once($dependency);
